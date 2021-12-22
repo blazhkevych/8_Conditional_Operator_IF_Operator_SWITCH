dd5cdfe5eeabb9f796ab98bab0e6bd6f79fb2f06
Виктор Блажкевич <blazhkevych.va@gmail.com>
2021-12-22 11:04:35 +0200

    2 задача

--- task_2/task_2.cpp
+++ task_2/task_2.cpp
@@ -11,26 +11,71 @@ int main()
 {
 	setlocale(LC_ALL, ""); //работает только с потоком вывода
 
-	cout << "Введите сумму в гривнах: " << endl;
-	double grn{ 0.0 };
-	cin >> grn;
-
-	cout << "\nВыберите валюту в которую будет переведена гривна: "
-		<< "\n1. Доллары."
-		<< "\n2. Евро." << endl;
-
-	char key{ 0 };
-	cin >> key;
-	cout << endl;
-
-	switch (key)
+	enum Currency
 	{
-	case '1':
-		cout << grn << " грн. = " << 0.037 * grn << " дол." << endl;
-		break;
+		Dollar = 1,
+		Euro = 2
+	};
 
-	case '2':
-		cout << grn << " грн. = " << 0.033 * grn << " евр." << endl;
+	cout << "Выберите вариант решения задачи: "
+		<< "\n1. Решение с использованием if "
+		<< "\n2. Решение с использованием switch " << endl;
+
+	int choice{ 0 };
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 1:
+	{
+		cout << "Введите сумму в гривнах: " << endl;
+		double grn{ 0.0 };
+		cin >> grn;
+
+		cout << "\nВыберите валюту в которую будет переведена гривна: "
+			<< "\n1. Доллары."
+			<< "\n2. Евро." << endl;
+
+		int key{ 0 };
+		cin >> key;
+		cout << endl;
+
+		if (key == Dollar)
+			cout << grn << " грн. = " << 0.037 * grn << " дол." << endl;
+		else if (key == Euro)
+			cout << grn << " грн. = " << 0.033 * grn << " евр." << endl;
+	}
+	break;
+
+	case 2:
+	{
+		cout << "Введите сумму в гривнах: " << endl;
+		double grn{ 0.0 };
+		cin >> grn;
+
+		cout << "\nВыберите валюту в которую будет переведена гривна: "
+			<< "\n1. Доллары."
+			<< "\n2. Евро." << endl;
+
+		int key{ 0 };
+		cin >> key;
+		cout << endl;
+
+		switch (key)
+		{
+		case Dollar:
+			cout << grn << " грн. = " << 0.037 * grn << " дол." << endl;
+			break;
+
+		case Euro:
+			cout << grn << " грн. = " << 0.033 * grn << " евр." << endl;
+			break;
+		}
+	}
+	break;
+
+	default:
+		cout << "Попробуйте еще раз !" << endl;
 		break;
 	}
 
