cef35ffa8c499f89e5a140bd0514be54886d66fb
Виктор Блажкевич <blazhkevych.va@gmail.com>
2021-12-22 12:36:46 +0200

    +5 задание

--- task_4/task_4.cpp
+++ task_4/task_4.cpp
@@ -3,13 +3,53 @@
 проверить корректность введённых значений.
 */
 
+/*
+с клавиатуры 6 значений по отдельности:
+дата (день, месяц, год), время (часы, минуты, секунды)
+*/
+
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, ""); //работает только с потоком вывода
+	setlocale(LC_ALL, ""); // работает только с потоком вывода
+
+	int date_day{ 0 }, date_month{ 0 }, date_year{ 0 }, // дата (день, месяц, год)
+		time_hours{ 0 }, time_minutes{ 0 }, time_seconds{ 0 }; // время (часы, минуты, секунды)
+
+	enum MyEnum
+	{
+		January = 1, // Январь 
+		February = 2, // Февраль 
+		March = 3, // Март 
+		April = 4, // Апрель 
+		May = 5, // Май 
+		June = 6, // Июнь 
+		July = 7, // Июль 
+		August = 8, // Август 
+		September = 9, // Сентябрь 
+		October = 10, // Октябрь 
+		November = 11, // Ноябрь 
+		December = 12 // Декабрь 
+	};
+
+	cout << "Введите год:" << endl;
+	cin >> date_year;
+	cout << "Введите месяц:" << endl;
+	cin >> date_month;
+	cout << "Введите день:" << endl;
+	cin >> date_day;
+
+	cout << "Введите часы:" << endl;
+	cin >> time_hours;
+	cout << "Введите минуты:" << endl;
+	cin >> time_minutes;
+	cout << "Введите день:" << endl;
+	cin >> time_seconds;
+
+
 
 
 
--- task_5/task_5.cpp
+++ task_5/task_5.cpp
@@ -1,6 +1,8 @@
 ﻿/*
 5. Ввести с клавиатуры номер трамвайного билета и определить является
 ли он счастливым.
+6 значное число
+счастливый - сумма первых 3 цифр равна сумме вторых 3 цифр
 */
 
 #include<iostream>
@@ -11,7 +13,22 @@ int main()
 {
 	setlocale(LC_ALL, ""); //работает только с потоком вывода
 
+	cout << "Введите номер трамвайного билетика (6значное число): " << endl;
+	int ticket_number{ 0 };
+	cin >> ticket_number;
 
+	int ticket_number_1 = ticket_number / 100000; // 1 цифра
+	int ticket_number_2 = ticket_number / 10000 % 10; // 2 цифра
+	int ticket_number_3 = ticket_number / 1000 % 10; // 3 цифра
+	int ticket_number_4 = ticket_number / 100 % 10; // 4 цифра
+	int ticket_number_5 = ticket_number / 10 % 10; // 5 цифра
+	int ticket_number_6 = ticket_number % 10; // 6 цифра
+
+	if ((ticket_number_1 + ticket_number_2 + ticket_number_3)
+		== (ticket_number_4 + ticket_number_5 + ticket_number_6))
+		cout << "Мои поздравления, у Вас счастливый билетик !" << endl;
+	else
+		cout << "У Вас обычный билетик." << endl;
 
 	return 0;
 }
