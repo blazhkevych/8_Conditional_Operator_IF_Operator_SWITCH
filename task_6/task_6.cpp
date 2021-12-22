dab3e5764fcee7c9b1aa7db120320df972f3a5aa
Виктор Блажкевич <blazhkevych.va@gmail.com>
2021-12-22 13:04:35 +0200

    +6 задание

--- task_6/task_6.cpp
+++ task_6/task_6.cpp
@@ -13,7 +13,45 @@ int main()
 {
 	setlocale(LC_ALL, ""); //работает только с потоком вывода
 
+	cout << "Программа расчета скидок." << endl;
 
+	cout << "Введите Количество товара: " << endl;
+	int quantity_of_goods{ 0 };
+	cin >> quantity_of_goods;
+
+	cout << "Введите стоимость за штуку: " << endl;
+	double cost_per_item{ 0.0 };
+	cin >> cost_per_item;
+
+	double total_cost_of_goods = quantity_of_goods * cost_per_item; // суммарная стоимость товара
+
+	if (total_cost_of_goods < 100)
+	{
+		cout << "У Вас нету скидки на товар." << endl;
+		cout << "Итоговая стоимость товара: "
+			<< total_cost_of_goods * 0.97 << " грн." << endl;
+	}
+	else if (total_cost_of_goods >= 100 && total_cost_of_goods < 200)
+	{
+		cout << "Стоимость товара: " << total_cost_of_goods << " грн." << endl;
+		cout << "Ваша скидка составляет 3%." << endl;
+		cout << "Итоговая стоимость товара, включая скидку: "
+			<< total_cost_of_goods * 0.97 << " грн." << endl;
+	}
+	else if (total_cost_of_goods >= 200 && total_cost_of_goods < 300)
+	{
+		cout << "Стоимость товара: " << total_cost_of_goods << " грн." << endl;
+		cout << "Ваша скидка составляет 5%." << endl;
+		cout << "Итоговая стоимость товара, включая скидку: "
+			<< total_cost_of_goods * 0.95 << " грн." << endl;
+	}
+	else if (total_cost_of_goods >= 300)
+	{
+		cout << "Стоимость товара: " << total_cost_of_goods << " грн." << endl;
+		cout << "Ваша скидка составляет 7%." << endl;
+		cout << "Итоговая стоимость товара, включая скидку: "
+			<< total_cost_of_goods * 0.93 << " грн." << endl;
+	}
 
 	return 0;
 }
